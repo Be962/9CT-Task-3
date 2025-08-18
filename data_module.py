33a72c2d2ebaeb3d188b0205332156058a8fdfb0
Be962 <benjamin.saunders23@education.nsw.gov.au>
2025-08-18 19:25:53 +1000

    Works perfectly, main.py and data_module.py

--- data_module.py
+++ data_module.py
@@ -7,6 +7,16 @@ df = df.drop(['Won', 'Tied', 'Draw', 'Balls', 'Column1'], axis=1)
 year = df.iloc[:,5]
 average = df.iloc[:, 3]
 
+def setup():
+    import pandas as pd
+    import matplotlib.pyplot as plt
+
+    df = pd.read_csv('Dataset.csv')
+    df = df.drop(['Won', 'Tied', 'Draw', 'Balls', 'Column1'], axis=1)
+    year = df.iloc[:,5]
+    average = df.iloc[:, 3]
+    return year, average
+
 def show_plot(year, average):
     plt.title('Runs Per Wicket (RPW) through time')
     plt.xlabel('Years')
@@ -16,15 +26,22 @@ def show_plot(year, average):
 
 def filter():
     start_years = 0
-    end_years = 0
-    while start_years < 1901 or start_years > 2025:
-        start_years = int(input('What year would you like to start as? (1901-2025)'))
-    while end_years < 1901 or end_years > 2025 and end_years < start_years:
-        end_years = int(input('What year would you like to end as? '))
-    end_years = end_years - 1907
-    start_years = start_years - 1901
+    end_years = 3000
+    while start_years < 1950 or start_years > 2024:
+        start_years = int(input('What year would you like to start as? (1950-2025) '))
+    while ((end_years < 1950 or end_years > 2025) or end_years <= start_years) or end_years == 0:
+        end_years = int(input('What year would you like to end as? (1950-2025) '))
+    end_years = end_years - 1945
+    start_years = start_years - 1946
     year = df.iloc[start_years:end_years, 5]
     average = df.iloc[start_years:end_years, 3]
     show_plot(year, average)
 
-filter()
+def show_other_stats():
+    stddf = df['Ave'].std()
+    stddf = stddf.round(4)
+    overall_average = df['Runs'].sum() / df['Wkts'].sum()
+    overall_average = overall_average.round(4)
+    return stddf, overall_average
+
+# print(show_other_stats)
--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-from data_module import filter, show_plot
-import pandas as pd
-import matplotlib.pyplot as plt
-
-df = pd.read_csv('Dataset.csv')
-df = df.drop(['Won', 'Tied', 'Draw', 'Balls', 'Column1'], axis=1)
-year = df.iloc[:,5]
-average = df.iloc[:, 3]
+from data_module import filter, show_plot, show_other_stats, setup
 
 while True:
-    choice = input('What would you like to choose? \n 1. Show the full data \n 2. Filter then show data \n ')
+    setup()
+    print('<--------User Interface-------->')
+    choice = input('What would you like to choose? \n 1. Show the full data \n 2. Filter then show data \n 3. Show other interesting stats \n 4. Exit program \n ')
     if choice == str(1):
+        year, average = setup()
         show_plot(year, average)
     if choice == str(2):
         filter()
+    if choice == str(3):
+        show_other_stats()
+        overallstd, overallave = show_other_stats()
+        print(f"Overall average: {overallave}")
+        print(f"Overall standard deviation: {overallstd}")
+        leave = input('Enter anything to go back: ')
+        if leave != None:
+            pass
+    if choice == str(4):
+        print('Exiting program')
+        break
+    else:
+        print("Please select a number between 1-4")
