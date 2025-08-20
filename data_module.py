e4b1782997c502ebe9c64cece0075086cf703b47
Be962 <benjamin.saunders23@education.nsw.gov.au>
2025-08-21 09:21:27 +1000

    Added comments to main.py and data_module.py

--- data_module.py
+++ data_module.py
@@ -1,13 +1,19 @@
+# Imports
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+# Setup/prepping the dataset
 
 df = pd.read_csv('Dataset.csv')
 df = df.drop(['Won', 'Tied', 'Draw', 'Column1'], axis=1)
 year = df.iloc[:,6]
 average = df.iloc[:,4]
 
-def setup():
+
+def setup():        # Put the setup into a function that can be used in main.py, to make it tidier
     import pandas as pd
     import matplotlib.pyplot as plt
 
@@ -17,33 +23,33 @@ def setup():
     average = df.iloc[:, 4]
     return year, average
 
-def show_plot(year, average):
+def show_plot(year, average): # Gets year and average input, plots graph using those parameters.
     plt.title('Runs Per Wicket (RPW) through time')
     plt.xlabel('Years')
     plt.ylabel('Average')
     plt.plot(year, average)
-    plt.grid()
+    plt.grid() # Adds grid lines
     plt.show()
 
-def filter():
+def filter(): # 
     start_years = 0
     end_years = 3000
     while start_years < 1946 or start_years > 2024:
         start_years = int(input('What year would you like to start as? (1950-2025) '))
     while ((end_years < 1946 or end_years > 2025) or end_years <= start_years) or end_years == 0:
         end_years = int(input('What year would you like to end as? (1950-2025) '))
-    end_years = end_years - 1945
+    end_years = end_years - 1945 # Because the indexing does not have 2025 rows, the input year needs to be converted into the row number
     start_years = start_years - 1946
-    year = df.iloc[start_years:end_years, 6]
+    year = df.iloc[start_years:end_years, 6]  # Sets the new year and average to use the input years
     average = df.iloc[start_years:end_years, 4]
-    show_plot(year, average)
+    show_plot(year, average) # Plots the new year and average
 
 def show_other_stats():
-    stddf = df['Ave'].std()
-    stddf = stddf.round(4)
-    overall_average = df['Runs'].sum() / df['Wkts'].sum()
-    overall_average = overall_average.round(4)
-    overall_rpo = df['Runs'].sum() / df['Balls'].sum()
-    overall_rpo = overall_rpo * 6
-    overall_rpo = overall_rpo.round(4)
-    return stddf, overall_average, overall_rpo
+    stddf = df['Ave'].std() # Finds the standard deviation of the 'Ave' column
+    stddf = stddf.round(4) # Rounds to 4 for tidiness
+    overall_average = df['Runs'].sum() / df['Wkts'].sum() # Overall average is all 'Runs' divided by all 'Wkts'
+    overall_average = overall_average.round(4) # Rounds to 4 for tidiness
+    overall_rpo = df['Runs'].sum() / df['Balls'].sum() # Runs per ball is all 'Runs' divided by alls 'Balls' 
+    overall_rpo = overall_rpo * 6 # Times by 6 to make it runs per over
+    overall_rpo = overall_rpo.round(4) # Rounds to 4 for tidiness
+    return stddf, overall_average, overall_rpo # Returns to be used in main.py
--- main.py
+++ main.py
@@ -5,21 +5,21 @@ while True:
     print("""<--------User Interface-------->""")
     choice = input('What would you like to choose? \n 1. Show the full data \n 2. Filter then show data \n 3. Show other interesting stats \n 4. Exit program \n ')
     if choice == str(1):
-        year, average = setup()
-        show_plot(year, average)
+        year, average = setup() # Gets the year and average from setup function
+        show_plot(year, average) # Plots the whole data from the first year/average 
     elif choice == str(2):
-        filter()
+        filter() # Calls the filter function
     elif choice == str(3):
-        show_other_stats()
+        show_other_stats() # Displays other stats
         overallstd, overallave, overall_rpo = show_other_stats()
-        print(f"Overall average: {overallave}")
-        print(f"Overall standard deviation: {overallstd}")
-        print(f"Overall runs per over: {overall_rpo}")
+        print(f"Overall average: {overallave}")                 # Prints other stats
+        print(f"Overall standard deviation: {overallstd}")      # Prints other stats
+        print(f"Overall runs per over: {overall_rpo}")          # Prints other stats
         leave = input('Enter anything to go back: ')
-        if leave != None:
+        if leave != None: # Any input will trigger this and go back to main loop
             pass
     elif choice == str(4):
         print('Exiting program')
-        break
+        break # Exits the program
     else:
-        print("Please select a number between 1-4")
+        print("Please select a number between 1-4") # Handling wrong inputs
